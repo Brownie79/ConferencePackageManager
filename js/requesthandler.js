a93ea7c34fdefd4fdb335359401fcd50ef1c0966
Spriha <spriha@dhcp194.merunorth101.iit.edu>
2016-09-26 00:36:13 -0500

    Updating requesthandler.js

--- js/requesthandler.js
+++ js/requesthandler.js
@@ -1,51 +1,57 @@
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ObjectID = require('mongodb').ObjectID;
+
+const url = 'mongodb://localhost:27017/cpm';
+
+createCollection("users");
+createCollection("conferences");
+
+
 module.exports = {
-    new_user : function(user_obj){        //// These are not objects, these functions. as in the functions you have below should go here in this space. 
-  	"user_id" : ObjectId(" "),
- 	"Name" : ,
+    new_user : function createCollection(users){
+    MongoClient.connect(url, (err,db) => { 
+        if(err) throw err;
+         
+    let coll1 = db.collection(users); 
+        
+    coll1.insertOne({
+    "Name" : ,
  	"Password" : ,
  	"Email" : ,
  	"Address" : ,
-     "Number" : ,
-     "Events" : []
-    },
-    new_conference : function(conference_obj){
-    "Event_name" : ObjectId(" "),
- 	"Location" : ,
- 	"Date" : ,
- 	"Schedule" :    
-    }
-}
-
-function createCollection(Users){
-    MongoClient.connect(url, (err,db) => { //you have to import MongoClient (see reset.js for imports)
-        if(err) throw err;
-        
-        db.createCollection(Users); // why are we creating a new collection? you should be INSERTing into an existing collection, not creation new ones
-        let coll = db.collection(Users); //this line is correct
-        fs.readFile(Users.JSON, (err, file)=>{ //what is "Users.JSON"? that variable doesn't exist (nor needs too) for this
-            if(err) throw err;
-            data = JSON.parse(file); // you don't need to parse anything, user_obj and conference_obj are going to be json objects already
-            coll.insertOne(data).then((res) =>{
+    "Number" : ,
+    "Events" : []
+                }).then((res) =>{
                 console.log("Users added: ", res); 
-            }); //this is correct
-        });
+            }); 
+        
         db.close();
     });
-}
-
-function createCollection(Conferences){
+}      
+  	
+    },
+    
+    new_conference ; function createCollection(conferences){
     MongoClient.connect(url, (err,db) => {
         if(err) throw err;
         
-        db.createCollection(Conferences);
-        let coll = db.collection(Conferences);
-        fs.readFile(conferences.JSON, (err, file)=>{
-            if(err) throw err;
-            data = JSON.parse(file); /
-            coll.insertOne(data).then((res) =>{
+        let coll2 = db.collection(conferences);
+
+    coll2.insertOne({ 
+    "Event_name" : ,
+ 	"Location" : ,
+ 	"Date" : ,
+ 	"Schedule" : }).then((res) =>{
                 console.log("Conferences added: ", res); 
             });
-        });
+        
         db.close();
     });
 }
+
+
+
+
+
