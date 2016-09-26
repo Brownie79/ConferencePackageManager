13fd2e243c5bf962c5372917f2f9d57a9a9146f9
dbharel <dbharel@hawk.iit.edu>
2016-09-26 01:59:29 -0500

    fixed tabs

--- js/requesthandler.js
+++ js/requesthandler.js
@@ -6,28 +6,28 @@ const url = 'mongodb://localhost:27017/cpm';
 
 module.exports = {
     new_user : function(user){
-    MongoClient.connect(url, (err,db) => { 
-        if(err) throw err;
-         
-        let coll1 = db.collection("users"); 
+        MongoClient.connect(url, (err,db) => { 
+            if(err) throw err;
             
-        coll1.insertOne(user).then((res) =>{
-                    console.log("User added: ", res); 
-                }); 
+            let coll1 = db.collection("users"); 
+                
+            coll1.insertOne(user).then((res) =>{
+                console.log("User added: ", res); 
+            }); 
             db.close();
         });
     },      
     
 
     new_conference : function(conference){
-    MongoClient.connect(url, (err,db) => {
+        MongoClient.connect(url, (err,db) => {
         if(err) throw err;
         
         let coll2 = db.collection("conferences");
 
-    coll2.insertOne(conference).then((res) =>{
-                console.log("Conference added: ", res); 
-            });
+        coll2.insertOne(conference).then((res) =>{
+            console.log("Conference added: ", res); 
+        });
         
         db.close();
     });
