5e4b8460b81a727870d0b88a309a4322f493273a
Spriha <spriha@dhcp78.merunorth97.iit.edu>
2016-10-02 16:40:58 -0500

    requesthandler updated

--- js/requesthandler.js
+++ js/requesthandler.js
@@ -1,18 +1,21 @@
+//imports
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectID = require('mongodb').ObjectID;
 
-const url = 'mongodb://localhost:27017/cpm';
+const url = 'mongodb://localhost:27017/cpm'; //points to the database we'll be using to read/write too
 
+//exports
 module.exports = {
     new_user : function(user){
         MongoClient.connect(url, (err,db) => { 
             if(err) throw err;
             
-            let coll1 = db.collection("users"); 
-                
+            let coll1 = db.collection("users"); //pre-defined collection users
+
+            //inserts new user into the collection "users" one by one   
             coll1.insertOne(user).then((res) =>{
-                console.log("User added: ", res); 
+                console.log("User added: ", res); //displays the doc added
             }); 
             db.close();
         });
@@ -23,13 +26,27 @@ module.exports = {
         MongoClient.connect(url, (err,db) => {
         if(err) throw err;
         
-        let coll2 = db.collection("conferences");
+        let coll2 = db.collection("conferences"); //pre-defined collection conferences
 
+        //inserts new conference into the collection "conferences" one by one 
         coll2.insertOne(conference).then((res) =>{
-            console.log("Conference added: ", res); 
+            console.log("Conference added: ", res); //displays the doc added
         });
         
         db.close();
     });
-}
+   },
+
+      search_user: function(user){
+       MongoClient.connect(url, (err,db) => {
+        if(err) throw err;
+        
+        let coll3 = db.collection("users"); //pre-defined collection users
+
+        //finds the logged-in user in the collection "users" and returns a cursor to it
+        coll3.findOne(user).then((res) =>{
+            console.log("User Found: ", res); //displays the doc found
+        });
+      }
+    }
 }
